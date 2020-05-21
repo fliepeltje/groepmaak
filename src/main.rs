9276aa3b6919b6242d1b9b88f2451dd4b6840596
fliepeltje <donatas@rasiukevicius.dev>
2020-05-21 12:50:27 +0200

    Add command handler logic
    The logic is comprised of:
    1. Generating a list of names from a given input file
    2. Randomly sorting this list
    3. Taking slices from the list to form groups
    4. Formating the list of groups to a tring
    5. Optionally writing output to file
    6. Printing the output to stdout

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,72 @@
+use rand::seq::SliceRandom;
+use rand::thread_rng;
+use std::fs;
 use std::path::PathBuf;
 use structopt::StructOpt;
 
 #[derive(Debug, StructOpt)]
 #[structopt(about = "Create sized groups from a list of names")]
 struct Opt {
-    #[structopt(parse(from_os_str), help = "File with all names to place into groups separated by breakpoints")]
+    #[structopt(
+        parse(from_os_str),
+        help = "Plaintext file with all names to place into groups separated by new lines"
+    )]
     names_file: PathBuf,
 
     #[structopt(help = "Size of groups to made")]
     group_size: u8,
 
-    #[structopt(parse(from_os_str), help = "Output file for groups, stdout if not specified", short = "out", long = "output-file")]
-    output: Option<PathBuf>
+    #[structopt(
+        parse(from_os_str),
+        help = "Output file for groups, stdout if not specified",
+        long = "out"
+    )]
+    output: Option<PathBuf>,
+}
+
+fn create_name_vector(path: &PathBuf) -> Vec<String> {
+    let file_content = fs::read_to_string(path).expect("Unable to read file");
+    let mut names: Vec<String> = file_content.lines().map(|s| s.to_owned()).collect();
+    names.shuffle(&mut thread_rng());
+    names
+}
+
+fn create_groups(names: Vec<String>, group_size: &u8) -> Vec<Vec<String>> {
+    let groups: Vec<Vec<String>> = names
+        .chunks(group_size.to_owned().into())
+        .map(|v| v.to_vec())
+        .collect();
+    groups
+}
+
+fn format_groups(groups: Vec<Vec<String>>) -> String {
+    let groups: Vec<String> = groups
+        .iter()
+        .enumerate()
+        .map(|(i, g)| format_single_group(g.to_vec(), i))
+        .collect();
+    let string = groups.join("\n\n");
+    string
+}
+
+fn format_single_group(group: Vec<String>, group_number: usize) -> String {
+    let mut body = vec![format!("Group {}:", group_number + 1)];
+    body.extend(group);
+    let group_string = body.join("\n");
+    group_string
 }
 
 fn main() {
     let opt = Opt::from_args();
-    println!("{:?}", opt);
+    let ordered_list = create_name_vector(&opt.names_file);
+    let shuffled_list = create_groups(ordered_list, &opt.group_size);
+    let group_output = format_groups(shuffled_list);
+    match opt.output {
+        Some(path) => {
+            fs::write(&path, &group_output).expect("Could not write to file");
+            println!("Succesfully wrote to file.");
+            println!("{}", group_output);
+        }
+        None => println!("{}", group_output),
+    }
 }
